3c0202ca9aed899f93284ee35b42878688fcf7ab
Mihael Kardash <mixan193@gmail.com>
2023-07-03 17:49:35 +0300

    Добавьте файлы проекта.

---
+++ 5.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteWords(SplitText(Console.ReadLine()));
+            Console.ReadKey();
+        }
+        private static string[] SplitText(string text)
+        {
+            return text.Split(' ');
+        }
+        private static void WriteWords(string[] text)
+        {
+            foreach (string word in text)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
